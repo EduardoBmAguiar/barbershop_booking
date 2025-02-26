bb4fef27dc8948e2a9598c1e0099748de778d544
Eduardo Benjamin <duduzzinxxz@gmail.com>
2025-02-26 13:09:25 -0300

    Project Created

---
+++ src/main/java/com/webedu/ben_barber/BenBarberApplication.java
@@ -0,0 +1,13 @@
+package com.webedu.ben_barber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenBarberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenBarberApplication.class, args);
+	}
+
+}
