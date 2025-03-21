df49d1ad75b22cd125dcd1e7b39cf7b7104208a2
Eduardo Benjamin <duduzzinxxz@gmail.com>
2025-03-21 13:43:56 -0300

    Log for UserService Added

--- src/main/java/com/webedu/ben_barber/services/UserService.java
+++ src/main/java/com/webedu/ben_barber/services/UserService.java
@@ -4,6 +4,7 @@ import com.webedu.ben_barber.entities.User;
 import com.webedu.ben_barber.exceptions.DatabaseException;
 import com.webedu.ben_barber.exceptions.ResourceNotFoundException;
 import com.webedu.ben_barber.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class UserService {
 
@@ -20,13 +22,16 @@ public class UserService {
 
     @Transactional
     public User addUser(User user) {
+        log.info("New User created");
         return userRepository.save(user);
     }
 
     @Transactional
     public User updateUser(Long id, User user) {
+        log.info("Finding user by Id");
         Optional<User> userOptional = userRepository.findById(id);
-
+        log.info("User found");
+        log.info("Updating User");
         if (userOptional.isPresent()) {
             User userToUpdate = userOptional.get();
 
@@ -34,6 +39,7 @@ public class UserService {
             if (!(user.getEmail() == null)) { userToUpdate.setEmail(user.getEmail()); }
             if (!(user.getPassword() == null)) { userToUpdate.setPassword(user.getPassword()); }
 
+            log.info("User updated");
             return userRepository.save(userToUpdate);
         } else {
             throw new RuntimeException("User not found");
@@ -42,21 +48,26 @@ public class UserService {
 
     @Transactional
     public List<User> findAllUsers() {
+        log.info("Finding all users");
         return userRepository.findAll();
     }
 
     @Transactional
     public User findById(Long id) {
+        log.info("Finding user by Id");
         Optional<User> userOptional = userRepository.findById(id);
+        log.info("User found");
         return userOptional.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     @Transactional
     public void delete(Long id) {
+        log.info("Deleting user by Id");
         userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
         try {
             userRepository.deleteById(id);
         } catch (DataIntegrityViolationException e) {
+            log.error("Integrity violation");
             throw new DatabaseException(e.getMessage());
         }
     }
