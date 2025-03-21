43d5ecac2e4bec1b3dd1e56a8ab3234afcedcd41
Eduardo Benjamin <duduzzinxxz@gmail.com>
2025-03-21 13:30:59 -0300

    Log for ResourceExceptionHandler Added

--- src/main/java/com/webedu/ben_barber/infra/ResourceExceptionHandler.java
+++ src/main/java/com/webedu/ben_barber/infra/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import com.webedu.ben_barber.exceptions.DatabaseException;
 import com.webedu.ben_barber.exceptions.InvalidDateException;
 import com.webedu.ben_barber.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,11 +12,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
 
+@Slf4j
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        log.error("Not Found");
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
@@ -25,6 +28,7 @@ public class ResourceExceptionHandler {
 
     @ExceptionHandler(DatabaseException.class)
     public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        log.error("Database error");
         String error = "Database error";
         HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
@@ -33,6 +37,7 @@ public class ResourceExceptionHandler {
 
     @ExceptionHandler(InvalidDateException.class)
     public ResponseEntity<StandardError> invalidDate(InvalidDateException e, HttpServletRequest request) {
+        log.error("Invalid date");
         String error = "Invalid date";
         HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
