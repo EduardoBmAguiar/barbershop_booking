7eb6c80923a82b2268a3aaa4acc2b61d33370f46
Eduardo Benjamin <duduzzinxxz@gmail.com>
2025-03-21 13:38:50 -0300

    Log for OptionService Added

--- src/main/java/com/webedu/ben_barber/services/OptionService.java
+++ src/main/java/com/webedu/ben_barber/services/OptionService.java
@@ -3,6 +3,7 @@ package com.webedu.ben_barber.services;
 import com.webedu.ben_barber.entities.Option;
 import com.webedu.ben_barber.exceptions.ResourceNotFoundException;
 import com.webedu.ben_barber.repositories.OptionRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class OptionService {
 
@@ -18,23 +20,32 @@ public class OptionService {
 
     @Transactional
     public Option addOptions(Option option) {
+        log.info("New Option created");
         return optionRepository.save(option);
     }
 
     @Transactional
     public List<Option> findAllOptions() {
+        log.info("Finding all Options in repository");
         return optionRepository.findAll();
     }
 
     @Transactional
     public Option updatePrice(Long id, Option newPrice) {
+        log.info("Finding Option by id");
         Option option = optionRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Id Not Found"));
+        log.info("Option Found");
+        log.info("Updating Option price");
         option.setPrice(newPrice.getPrice());
+        log.info("New Option price updated");
         return optionRepository.save(option);
     }
 
     public void deleteOption(Long id) {
+        log.info("Finding Option by id");
         Option option = optionRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Id Not Found"));
+        log.info("Option Found");
+        log.info("Deleting Option");
         try {
             optionRepository.deleteById(id);
         } catch (DataIntegrityViolationException e) {
