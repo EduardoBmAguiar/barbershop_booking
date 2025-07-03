c68ff6d554e081826828b16b6412d19acf5c4df3
Eduardo Benjamin <eduardo.bm.aguiar@gmail.com>
2025-07-03 12:04:19 -0300

    refactor(mappers): Convert static mappers to Spring @Component beans

--- src/main/java/com/webedu/ben_barber/mapper/client/ClientMapper.java
+++ src/main/java/com/webedu/ben_barber/mapper/client/ClientMapper.java
@@ -3,14 +3,16 @@ package com.webedu.ben_barber.mapper.client;
 import com.webedu.ben_barber.dto.client.ClientRequestDTO;
 import com.webedu.ben_barber.dto.client.ClientResponseDTO;
 import com.webedu.ben_barber.entities.Client;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ClientMapper {
 
-    public static ClientResponseDTO toDTO(Client entity) {
+    public ClientResponseDTO toDTO(Client entity) {
         return new ClientResponseDTO(entity.getId(), entity.getName(), entity.getEmail());
     }
 
-    public static Client toEntity(ClientRequestDTO dto) {
+    public Client toEntity(ClientRequestDTO dto) {
         return new Client(null, dto.name(), dto.email(), dto.password());
     }
 }
--- src/main/java/com/webedu/ben_barber/mapper/option/OptionMapper.java
+++ src/main/java/com/webedu/ben_barber/mapper/option/OptionMapper.java
@@ -3,14 +3,16 @@ package com.webedu.ben_barber.mapper.option;
 import com.webedu.ben_barber.dto.option.OptionRequestDTO;
 import com.webedu.ben_barber.dto.option.OptionResponseDTO;
 import com.webedu.ben_barber.entities.Option;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OptionMapper {
 
-    public static OptionResponseDTO toDTO(Option entity) {
+    public OptionResponseDTO toDTO(Option entity) {
         return new OptionResponseDTO(entity.getId(), entity.getName(), entity.getPrice());
     }
 
-    public static Option toEntity(OptionRequestDTO dto) {
+    public Option toEntity(OptionRequestDTO dto) {
         return new Option(null, dto.name(), dto.price());
     }
 }
