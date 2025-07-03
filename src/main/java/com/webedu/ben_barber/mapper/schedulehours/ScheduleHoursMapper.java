10a30505ca8484ab3e681c587c0b088011a483c4
Eduardo Benjamin <eduardo.bm.aguiar@gmail.com>
2025-07-03 12:20:22 -0300

    refactor(mappers): Convert static mappers to Spring @Component beans - Update

--- src/main/java/com/webedu/ben_barber/mapper/schedulehours/ScheduleHoursMapper.java
+++ src/main/java/com/webedu/ben_barber/mapper/schedulehours/ScheduleHoursMapper.java
@@ -3,13 +3,19 @@ package com.webedu.ben_barber.mapper.schedulehours;
 import com.webedu.ben_barber.dto.schedulehours.ScheduleHoursResponseDTO;
 import com.webedu.ben_barber.entities.ScheduleHours;
 import com.webedu.ben_barber.mapper.barber.BarberMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ScheduleHoursMapper {
 
-    public static ScheduleHoursResponseDTO toDTO(ScheduleHours entity) {
+    @Autowired
+    BarberMapper barberMapper;
+
+    public ScheduleHoursResponseDTO toDTO(ScheduleHours entity) {
         return new ScheduleHoursResponseDTO(entity.getId(),
                 entity.getDate(),
                 entity.getHourTime(),
-                BarberMapper.toDTO(entity.getBarber()));
+                barberMapper.toDTO(entity.getBarber()));
     }
 }
