178dc635f45c4bc6e0e604cca1c94a53c176744a
Eduardo Benjamin <eduardo.bm.aguiar@gmail.com>
2025-07-09 12:37:04 -0300

    refactor(entities): remove unusable imports

--- src/main/java/com/webedu/ben_barber/entities/Client.java
+++ src/main/java/com/webedu/ben_barber/entities/Client.java
@@ -3,9 +3,6 @@ package com.webedu.ben_barber.entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.ArrayList;
--- src/main/java/com/webedu/ben_barber/entities/Option.java
+++ src/main/java/com/webedu/ben_barber/entities/Option.java
@@ -2,8 +2,6 @@ package com.webedu.ben_barber.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.math.BigDecimal;
