76138a1623d50178d0639115340633bf4d392cfa
Eduardo Benjamin <eduardo.bm.aguiar@gmail.com>
2025-07-09 12:11:56 -0300

    feat(dto): adding validation notes in TimeBlockRequestDTO

--- src/main/java/com/webedu/ben_barber/dto/TimeBlock/TimeBlockRequestDTO.java
+++ src/main/java/com/webedu/ben_barber/dto/TimeBlock/TimeBlockRequestDTO.java
@@ -1,12 +1,15 @@
 package com.webedu.ben_barber.dto.TimeBlock;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public record TimeBlockRequestDTO(
-        Long barberId,
-        LocalDate date,
-        LocalTime startTime,
-        LocalTime endTime,
-        String reason
-) {}
+public record TimeBlockRequestDTO(@NotNull Long barberId,
+                                  @NotNull LocalDate date,
+                                  @NotNull LocalTime startTime,
+                                  @NotNull LocalTime endTime,
+                                  @NotBlank String reason
+) {
+}
